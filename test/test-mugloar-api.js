435354f8720323b66b7e0d73a449711369af5b37
dward1466 <dward@globesherpa.com>
2016-07-23 20:50:01 -0700

    Test running, starting to use eslint

--- test/test-mugloar-api.js
+++ test/test-mugloar-api.js
@@ -1,47 +1,76 @@
 'use strict';
 
-const Promise = require('bluebird')
-const chai = require('chai')
-const request = require('supertest-as-promised');
+const Promise = require('bluebird');
+const chai = require('chai');
 const mugloar = require('../lib/mugloar-api');
-const xml2js = Promise.promisifyAll(require('xml2js')).Parser({explicitArray: false});
-//const parser = new xml2js.Parser({explicitArray: false});
+const xml2js = Promise.promisifyAll(require('xml2js'));
+const parser = new xml2js.Parser({explicitArray: false});
 
 const expect = chai.expect;
 
 describe('Mugloar API', () => {
-  let gameId = 7795598;
-
+  const gameBody =
+  {
+    gameId: 7795598,
+    knight: {
+      name: 'Sir. Norman Hale of Nunavut',
+      attack: 0,
+      armor: 8,
+      agility: 4,
+      endurance: 8
+    }
+  };
   // beforeEach(() => {
   //   gameId = 7795598;
   // });
+  describe('#getNewGame()', () => {
+    let body = {};
+    it('should reply successfully', () => mugloar.getNewGame()
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('object');
+        body = res.body;
+        //console.log(body);
+      })
+    );
 
-  it.skip('should get a valid new game', () => mugloar.getNewGame()
-    .expect(200)
-    .expect('Content-Type', /json/)
-    .expect(res => {
-      expect(res.body).to.be.an('object');
-      expect(res.body.gameId).to.be.a('number');
-      expect(res.body.knight).to.be.an('object');
-      expect(res.body.knight.attack).to.be.a('number');
-      expect(res.body.knight.armor).to.be.a('number');
-      expect(res.body.knight.agility).to.be.a('number');
-      expect(res.body.knight.endurance).to.be.a('number');
-      this.gameId = res.body.gameId;
-      console.log('Got gameId: ' + this.gameId + '\n');
+    it('should have a valid body', () => {
+      expect(body.gameId).to.be.a('number');
+      expect(body.knight).to.be.an('object');
+      expect(body.knight.attack).to.be.a('number');
+      expect(body.knight.armor).to.be.a('number');
+      expect(body.knight.agility).to.be.a('number');
+      expect(body.knight.endurance).to.be.a('number');
     })
-  );
 
-  it('should get the weather with given gameId', () => mugloar.getWeather(gameId)
-    .expect(200)
-    .expect('Content-Type', /xml/)
-    .expect(res => xml2js.parseStringAsync(res.text)
-      .then(xml => {
-        console.log('Current gameId: ' + this.gameId + '\n');
-        console.log(xml);
-        //expect(xml.report.code).to.be.a('string');
-        return expect(xml.report.code).to.equal('NMR');
-      }))
+  });
 
-  );
+  describe('#getWeather()', () => {
+    let body;
+    it('should reply successfully', () => mugloar.getWeather(gameBody.gameId)
+      .expect(200)
+      .expect('Content-Type', /xml/)
+      .then(res => {
+        expect(res.text).to.be.a('string');
+        body = res.text;
+        //console.log(body);
+      })
+    );
+    it('should convert xml to json', () => {
+      return parser.parseStringAsync(body)
+        .then(xml => {
+          //console.log(xml);
+          expect(xml.report).to.be.an('object');
+          expect(xml.report.code).to.be.a('string');
+          expect(xml.report.code).to.have.lengthOf(3);
+          body = xml;
+        });
+    });
+  });
+
+  describe('#solveGame()', () => {
+
+  });
+
 });
