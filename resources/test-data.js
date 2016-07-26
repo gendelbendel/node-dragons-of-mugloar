b545f8b81b03903a3019ea7d8d2a41ef64c645ae
dward1466 <dward@globesherpa.com>
2016-07-26 02:02:43 -0700

    Improve test coverage, update README.md, address bugs in API

---
+++ resources/test-data.js
@@ -0,0 +1,102 @@
+'use strict';
+
+exports.gameData = {
+  zen: {
+    gameId: 7795598,
+    knight: {
+      name: 'Sir. Norman Hale of Nunavut',
+      attack: 0,
+      armor: 8,
+      agility: 4,
+      endurance: 8
+    },
+    dragon: {
+      dragon: {
+        scaleThickness: 5,
+        clawSharpness: 5,
+        wingStrength: 5,
+        fireBreath: 5
+      }
+    },
+    report: {
+      code: 'T E'
+    }
+  },
+  nmr: {
+    gameId: 5158282,
+    knight: {
+      name: "Sir. Marcus Soto of Northwest Territories",
+      attack: 2,
+      armor: 5,
+      agility: 6,
+      endurance: 7
+    },
+    dragon: {
+      dragon: {
+        scaleThickness: 1,
+        clawSharpness: 6,
+        wingStrength: 4,
+        fireBreath: 9
+      }
+    },
+    report: {
+      code: 'NMR'
+    }
+  },
+  hva: {
+    gameId: 6969568,
+    knight: {
+      name: 'Sir. Chad Luna of Prince Edward Island',
+      attack: 5,
+      armor: 3,
+      agility: 5,
+      endurance: 7
+    },
+    dragon: {
+      dragon: {
+        scaleThickness: 10,
+        clawSharpness: 10,
+        wingStrength: 0,
+        fireBreath: 0
+      }
+    },
+    report: {
+      code: 'HVA'
+    }
+  },
+  sro: {
+    gameId: 4804977,
+    knight: {
+      name: "Sir. Chris Willis of Saskatchewan",
+      attack: 5,
+      armor: 5,
+      agility: 8,
+      endurance: 2
+    },
+    dragon: {},
+    report: {
+      code: 'SRO'
+    }
+  },
+  fog: {
+    gameId: 12323,
+    knight: {
+      name: "Sir. Kyle Bowers of Quebec",
+      attack: 6,
+      armor: 7,
+      agility: 3,
+      endurance: 4
+    },
+    dragon: {
+      dragon: {
+        scaleThickness: 10,
+        clawSharpness: 10,
+        wingStrength: 0,
+        fireBreath: 0
+      }
+    },
+    report: {
+      code: 'FUNDEFINEDG' // don't ask me
+    }
+  }
+}
--- test/test-mugloar-api.js
+++ test/test-mugloar-api.js
@@ -3,27 +3,15 @@
 const Promise = require('bluebird');
 const chai = require('chai');
 const mugloar = require('../lib/mugloar-api');
+const testData = require('../resources/test-data.js').gameData;
 const xml2js = Promise.promisifyAll(require('xml2js'));
-const parser = new xml2js.Parser({explicitArray: false});
 
+const parser = new xml2js.Parser({explicitArray: false});
 const expect = chai.expect;
 
 describe('Mugloar API', () => {
-  const gameBody =
-  {
-    gameId: 7795598,
-    knight: {
-      name: 'Sir. Norman Hale of Nunavut',
-      attack: 0,
-      armor: 8,
-      agility: 4,
-      endurance: 8
-    }
-  };
-  // beforeEach(() => {
-  //   gameId = 7795598;
-  // });
-  describe('#getNewGame()', () => {
+
+  context('#getNewGame()', () => {
     let body = {};
     it('should reply successfully', () => mugloar.getNewGame()
       .expect(200)
@@ -31,7 +19,6 @@ describe('Mugloar API', () => {
       .then(res => {
         expect(res.body).to.be.an('object');
         body = res.body;
-        //console.log(body);
       })
     );
 
@@ -42,35 +29,179 @@ describe('Mugloar API', () => {
       expect(body.knight.armor).to.be.a('number');
       expect(body.knight.agility).to.be.a('number');
       expect(body.knight.endurance).to.be.a('number');
-    })
-
-  });
-
-  describe('#getWeather()', () => {
-    let body;
-    it('should reply successfully', () => mugloar.getWeather(gameBody.gameId)
-      .expect(200)
-      .expect('Content-Type', /xml/)
-      .then(res => {
-        expect(res.text).to.be.a('string');
-        body = res.text;
-        //console.log(body);
-      })
-    );
-    it('should convert xml to json', () => {
-      return parser.parseStringAsync(body)
-        .then(xml => {
-          //console.log(xml);
-          expect(xml.report).to.be.an('object');
-          expect(xml.report.code).to.be.a('string');
-          expect(xml.report.code).to.have.lengthOf(3);
-          body = xml;
-        });
     });
   });
 
-  describe('#solveGame()', () => {
+  context('#getExistingGame()', () => {
+    let body = {};
+    let data = {};
+    before(() => {
+      data = testData.zen;
+    });
+    it('should reply successfully', () => mugloar.getExistingGame(data.gameId)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('object');
+        body = res.body;
+      })
+    );
+
+    it('should have a valid body', () => {
+      expect(body.gameId).to.be.a('string');
+      expect(body.knight).to.be.an('object');
+      expect(body.knight.attack).to.be.a('number');
+      expect(body.knight.armor).to.be.a('number');
+      expect(body.knight.agility).to.be.a('number');
+      expect(body.knight.endurance).to.be.a('number');
+    });
+
+    it('should have a body that matches saved details', () => {
+      // Commented out until/if possible unintended behvavior is addressed;
+      // See README.md Bugs
+      // expect(body.gameId).to.equal(data.gameId);
+
+      // If the above is commented, remove this next line
+      expect(body.gameId).to.equal(data.gameId.toString());
+      expect(body.knight.attack).to.equal(data.knight.attack);
+      expect(body.knight.armor).to.equal(data.knight.armor);
+      expect(body.knight.agility).to.equal(data.knight.agility);
+      expect(body.knight.endurance).to.equal(data.knight.endurance);
+    });
+  });
+
+  context('#getWeather()', () => {
+    let body;
+    let data;
+    context('NMR weather', () => {
+      before(() => {
+        data = testData.nmr;
+      });
+      it('should reply successfully', () => mugloar.getWeather(data.gameId)
+        .expect(200)
+        .expect('Content-Type', /xml/)
+        .then(res => {
+          expect(res.text).to.be.a('string');
+          body = res.text;
+        })
+      );
+      it('should convert xml to json', () =>
+        parser.parseStringAsync(body)
+          .then(xml => {
+            expect(xml.report).to.be.an('object');
+            body = xml;
+          })
+      );
+      it('should contain expected weather code', () => {
+        expect(body.report.code).to.be.a('string');
+        expect(body.report.code).to.equal(data.report.code);
+        expect(body.report.code).to.have.lengthOf(3);
+      });
+    });
+    context('T E weather', () => {
+      before(() => {
+        data = testData.zen;
+      });
+      it('should reply successfully', () => mugloar.getWeather(data.gameId)
+        .expect(200)
+        .expect('Content-Type', /xml/)
+        .then(res => {
+          expect(res.text).to.be.a('string');
+          body = res.text;
+        })
+      );
+      it('should convert xml to json', () =>
+        parser.parseStringAsync(body)
+          .then(xml => {
+            expect(xml.report).to.be.an('object');
+            body = xml;
+          })
+      );
+      it('should contain expected weather code', () => {
+        expect(body.report.code).to.be.a('string');
+        expect(body.report.code).to.equal(data.report.code);
+        expect(body.report.code).to.have.lengthOf(3);
+      });
+    });
+    context('SRO weather', () => {
+      before(() => {
+        data = testData.sro;
+      });
+      it('should reply successfully', () => mugloar.getWeather(data.gameId)
+        .expect(200)
+        .expect('Content-Type', /xml/)
+        .then(res => {
+          expect(res.text).to.be.a('string');
+          body = res.text;
+        })
+      );
+      it('should convert xml to json', () =>
+        parser.parseStringAsync(body)
+          .then(xml => {
+            expect(xml.report).to.be.an('object');
+            body = xml;
+          })
+      );
+      it('should contain expected weather code', () => {
+        expect(body.report.code).to.be.a('string');
+        expect(body.report.code).to.equal(data.report.code);
+        expect(body.report.code).to.have.lengthOf(3);
+      });
+    });
+    context('HVA weather', () => {
+      before(() => {
+        data = testData.hva;
+      });
+      it('should reply successfully', () => mugloar.getWeather(data.gameId)
+        .expect(200)
+        .expect('Content-Type', /xml/)
+        .then(res => {
+          expect(res.text).to.be.a('string');
+          body = res.text;
+        })
+      );
+      it('should convert xml to json', () =>
+        parser.parseStringAsync(body)
+          .then(xml => {
+            expect(xml.report).to.be.an('object');
+            body = xml;
+          })
+      );
+      it('should contain expected weather code', () => {
+        expect(body.report.code).to.be.a('string');
+        expect(body.report.code).to.equal(data.report.code);
+        expect(body.report.code).to.have.lengthOf(3);
+      });
+    });
+    context('FOG (FUNDEFINEDG) weather', () => {
+      before(() => {
+        data = testData.fog;
+      });
+      it('should reply successfully', () => mugloar.getWeather(data.gameId)
+        .expect(200)
+        .expect('Content-Type', /xml/)
+        .then(res => {
+          expect(res.text).to.be.a('string');
+          body = res.text;
+        })
+      );
+      it('should convert xml to json', () =>
+        parser.parseStringAsync(body)
+          .then(xml => {
+            expect(xml.report).to.be.an('object');
+            body = xml;
+          })
+      );
+      it('should contain expected weather code', () => {
+        expect(body.report.code).to.be.a('string');
+        expect(body.report.code).to.equal(data.report.code);
+        expect(body.report.code).to.have.lengthOf(3);
+      });
+    });
 
   });
 
+  context('#solveGame()', () => {
+
+  });
 });
